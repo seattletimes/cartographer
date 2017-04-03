eab0b3b61b3d1b4db250299ba9a153d29a67216a
Thomas Wilburn <news@thomaswilburn.net>
2017-04-03 10:25:36 -0700

    Viable

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
 var input = document.querySelector(".search");
 var go = document.querySelector(".go");
-var mapElement = document.querySelector("leaflet-map");
-var map = mapElement.map;
+var layerSelect = document.querySelector(".layer-select");
+var popupInput = document.querySelector(".popup-input");
 
-map.setMaxZoom(15);
+var mapElement = document.querySelector("leaflet-map");
+var leaflet = mapElement.leaflet;
+var map = mapElement.map;
+var lookup = mapElement.lookup;
+
+map.setMaxZoom(17);
+
+var tiles = {};
+
+for (var l in lookup) {
+  var layer = lookup[l];
+  if (!(layer instanceof leaflet.TileLayer)) continue;
+  tiles[l] = layer;
+  map.removeLayer(layer);
+  var option = document.createElement("option");
+  option.value = l;
+  option.innerHTML = l;
+  layerSelect.appendChild(option);
+}
+
+var changeLayer = function() {
+  var value = layerSelect.value;
+  var layer = tiles[value];
+  layer.addTo(map);
+  for (var t in tiles) {
+    if (tiles[t] !== layer && tiles[t]._map) {
+      map.removeLayer(tiles[t]);
+    }
+  }
+};
+
+layerSelect.addEventListener("change", changeLayer);
+changeLayer();
+
+var bubble = new leaflet.Popup();
+
+map.on("click", function(e) {
+  bubble.setContent(popupInput.value.trim() || " ");
+  map.openPopup(bubble, e.latlng);
+});
+
+popupInput.addEventListener("keyup", function() {
+  var value = popupInput.value.trim() || " ";
+  bubble.setContent(value);
+});
 
 go.addEventListener("click", function() {
   var address = input.value.replace(/\s/g, "+");
